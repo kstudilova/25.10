0f466c03b37ca21f3e8c023f528040458ac909ba
kstudilova <kstudilova@gmail.com>
2025-10-25 21:13:08 +0300

    Good process

---
+++ 25.10/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main() {
+	using u_t = unsigned;
+	u_t a = 0;
+	size_t count = 0;
+	while (std::cin >> a) {
+		count ++;
+	}
+
+	if (std::cin.eof ()) {
+		std::cout << count;
+		std::cout << "\n";
+	} else if (std::cin.fail()){
+		std::cerr << "Error\n";
+		return 1;
+	}
+
+}
